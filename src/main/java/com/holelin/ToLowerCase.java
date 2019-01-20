762a4a7eddc785767f83ca332025721903f6bdf8
HoleLin <HoleLin@163.com>
2019-01-20 14:43:41 +0800

    709. To Lower Case

---
+++ src/main/java/com/holelin/ToLowerCase.java
@@ -0,0 +1,42 @@
+package com.holelin;
+
+import java.util.Arrays;
+
+/**
+ * ClassName: ToLowerCase
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ * Example 1:
+ * <p>
+ * Input: "Hello"
+ * Output: "hello"
+ * Example 2:
+ * <p>
+ * Input: "here"
+ * Output: "here"
+ * Example 3:
+ * <p>
+ * Input: "LOVELY"
+ * Output: "lovely"
+ *
+ * @author HoleLin
+ * @version 1.0
+ * @date 2019/1/20
+ */
+
+public class ToLowerCase {
+    public static String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i]>='A' && chars[i]<= 'Z') {
+                chars[i] += 32;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("HADJKHASDH"));
+        System.out.println(toLowerCase("asdjasASDASDdasd"));
+        System.out.println(toLowerCase("asdjasjJJJJJ"));
+    }
+}
