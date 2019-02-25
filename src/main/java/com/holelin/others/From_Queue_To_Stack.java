e31945bf5fa4221ac7d04297541e453d0c5a78cc
HoleLin <HoleLin@163.com>
2019-02-25 14:49:37 +0800

    * 仅用队列结构实现栈结构 * 仅用栈结构实现队列

---
+++ src/main/java/com/holelin/others/From_Queue_To_Stack.java
@@ -0,0 +1,74 @@
+package com.holelin.others;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * ClassName: From_Queue_To_Stack
+ * 仅用队列结构实现栈结构
+ * -- 利用两个Queue,
+ * ---- 当调用push()时,调用queue.add()
+ * ---- 当调用pop()时,将queue中的数据保留一个,其余加入help中,再poll queue中的元素(只有一个),最后交换queue和help的引用,返回之前queue.poll()的值
+ * ---- 当调用peek(),将queue中的数据保留一个,其余加入help中,再poll queue中的元素(只有一个),在将之前queue.poll()的值添加到help中,最后交换queue和help的引用,返回之前queue.poll()的值
+ * @author HoleLin
+ * @version 1.0
+ * @date 2019/2/25
+ */
+
+public class From_Queue_To_Stack<E> {
+	private Queue<E> queue;
+	private Queue<E> help;
+
+	public From_Queue_To_Stack() {
+		queue = new LinkedList<>();
+		help = new LinkedList<>();
+	}
+
+	public void push(E newData) {
+		queue.add(newData);
+	}
+
+	public E pop() {
+		if (queue.isEmpty()) {
+			throw new IllegalArgumentException("Stack is empty");
+		}
+		while (queue.size() > 1) {
+			help.add(queue.poll());
+		}
+		E ret = queue.poll();
+		swap();
+		return ret;
+	}
+
+	private void swap() {
+		Queue<E> tmp = help;
+		help = queue;
+		queue = tmp;
+
+	}
+
+	public E peek() {
+		if (queue.isEmpty()) {
+			throw new IllegalArgumentException("Stack is empty");
+		}
+		while (queue.size() != 1) {
+			help.add(queue.poll());
+		}
+		E ret = queue.poll();
+		help.add(ret);
+		swap();
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		From_Queue_To_Stack<Integer> stack = new From_Queue_To_Stack();
+		int[] nums = {10, 1, 4, 6, 5, -1, 3, 11, 61, 6};
+
+		for (int i = 0; i <nums.length; i++) {
+			stack.push(nums[i]);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(stack.pop()+" ");
+		}
+	}
+}
---
+++ src/main/java/com/holelin/others/From_Stack_To_Queue.java
@@ -0,0 +1,68 @@
+package com.holelin.others;
+
+import java.util.Stack;
+
+/**
+ * ClassName: From_Stack_To_Queue
+ * 仅用栈结构实现队列
+ * -- 利用两个栈pushStack和popStack,pushStack用于添加数据,popStack用于弹出数据;
+ * -- 操作:将pushStack中的数据倒入popStack
+ * -- 倒入条件
+ * 1. 当popStack为空
+ * 2. 倒入要全部倒入;
+ *
+ * @author HoleLin
+ * @version 1.0
+ * @date 2019/2/25
+ */
+
+public class From_Stack_To_Queue<E> {
+	private Stack<E> pushStack;
+	private Stack<E> popStack;
+
+	public From_Stack_To_Queue() {
+		pushStack = new Stack<>();
+		popStack = new Stack<>();
+	}
+
+	public void push(E newDta) {
+		pushStack.push(newDta);
+		importTo();
+	}
+
+	private void importTo() {
+		if (popStack.isEmpty()) {
+			while (!pushStack.isEmpty()) {
+				popStack.push(pushStack.pop());
+			}
+		}
+	}
+
+	public E pop() {
+
+		if (popStack.isEmpty() && pushStack.isEmpty()) {
+			throw new RuntimeException("Queue is empty");
+		}
+		importTo();
+		return popStack.pop();
+	}
+
+	public E peek() {
+		if (popStack.isEmpty() && pushStack.isEmpty()) {
+			throw new RuntimeException("Queue is empty");
+		}
+		importTo();
+		return popStack.peek();
+	}
+
+	public static void main(String[] args) {
+		From_Stack_To_Queue queue=new From_Stack_To_Queue();
+		int[] nums = {10, 1, 4, 6, 5, -1, 3, 11, 61, 6};
+		for (int i = 0; i <nums.length; i++) {
+			queue.push(nums[i]);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(queue.pop()+" ");
+		}
+	}
+}
